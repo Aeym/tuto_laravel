7997dc645af4e5db33ca276d8a42cd792499c816
Aeym <alrick.eymauzy@gmail.com>
2020-01-03 11:35:05 +0100

    tuto laravel 20%

---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'WelcomeController@index');
+
+Route::get('article/{n}', 'ArticleController@show')->where('n', '[0-9]+');
+// Route::get('1', function() { return 'Je suis la page 1 !'; });
+// Route::get('2', function() { return 'Je suis la page 2 !'; });
+// Route::get('3', function() { return 'Je suis la page 3 !'; });
+
+Route::get('{n}', function($n) { 
+    return response('Je suis la page ' . $n . ' !', 200);
+})->where('n', '[1-3]');
+
+// Route::get('article/{n}', function($n) {
+//     return view('article')->withNumero($n);
+// })->where('n', '[0-9]+');
+
+Route::get('facture/{n}', function($n) { 
+    return view('facture')->withNumero($n); 
+})->where('n', '[0-9]+');
+
+Route::get('users', 'UsersController@getInfos');
+Route::post('users', 'UsersController@postInfos');
+
+Route::get('contact', 'ContactController@getForm');
+Route::post('contact', 'ContactController@postForm');
